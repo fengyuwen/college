c0d4bd2e4f80cda60568ffc0c7a44bd804be9b63
123 <1231>
2018-07-05 16:41:09 +0800

    123

---
+++ app/src/main/java/com/mmvtc/college/activity/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.mmvtc.college.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mmvtc.college.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {//两秒跳转activity
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
