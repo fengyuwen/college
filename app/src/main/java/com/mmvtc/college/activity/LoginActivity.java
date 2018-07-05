e086f3d485be0351f514b820b081af658c5def9a
feng <84@qq.com>
2018-07-05 15:31:47 +0800

    1

--- app/src/main/java/com/mmvtc/college/activity/LoginActivity.java
+++ app/src/main/java/com/mmvtc/college/activity/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.mmvtc.college.R;
 import com.mmvtc.college.Utils.Local;
@@ -16,6 +17,7 @@ import com.mmvtc.college.Utils.Local;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -58,14 +60,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("GET");
                     conn.setConnectTimeout(5000);
-                    if (!cookie.isEmpty()) conn.setRequestProperty("Cookie", "ASP.NET_SessionId="+cookie);
+                    if (!cookie.isEmpty())
+                        conn.setRequestProperty("Cookie", cookie);
                     int code = conn.getResponseCode();
                     if (code == 200) {
                         if (cookie.isEmpty()) {
                             cookie = conn.getHeaderField("Set-Cookie");
-                            Log.i(TAG, "run: "+cookie);
-                            cookie = cookie.substring(cookie.indexOf("=")+1, cookie.indexOf(";"));
-                            Log.i(TAG, "run: "+cookie);
+                            Log.i(TAG, "run: " + cookie);
+                            cookie = cookie.substring(0, cookie.indexOf(";"));
                         }
                         InputStream is = conn.getInputStream();
                         final Bitmap bitmap = BitmapFactory.decodeStream(is);
@@ -96,8 +98,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 picUpdate();
                 break;
             case R.id.btn_login:
-                login();
-                picUpdate();
+                //需要改良
+                if (user.isEmpty() || passwrod.isEmpty() || vertift.isEmpty()) {
+                    Toast.makeText(this, "账号或秘密或验证码不能空", Toast.LENGTH_SHORT).show();
+                } else
+                    login();
                 break;
         }
     }
@@ -107,34 +112,49 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             @Override
             public void run() {
                 try {
-                    Document doc = Jsoup.connect(Local.url+"default2.aspx")
-                            .cookie("ASP.NET_SessionId",cookie)
-                            .data("__VIEWSTATE","dDw3OTkxMjIwNTU7Oz5qFv56B08dbR82AMSOW+P8WDKexA==")
-                            .data("TextBox1",user)
-                            .data("TextBox2",passwrod)
-                            .data("TextBox3",vertift)
-                            .data("RadioButtonList1","学生")
-                            .data("Button1","")
+                    Document doc = Jsoup.connect(Local.url + "default2.aspx")
+                            .cookie("ASP.NET_SessionId", cookie.substring(cookie.indexOf("=") + 1, cookie.length()))
+                            .data("__VIEWSTATE", "dDw3OTkxMjIwNTU7Oz5qFv56B08dbR82AMSOW+P8WDKexA==")
+                            .data("TextBox1", user)
+                            .data("TextBox2", passwrod)
+                            .data("TextBox3", vertift)
+                            .data("RadioButtonList1", "学生")
+                            .data("Button1", "")
                             .timeout(3000)
                             .post();
                     Element body = doc.body();
-                  /*if (true){
+                    Element script = doc.getElementsByTag("script").last();
+                    final String msg =  massage(script.html());
+                    if (msg.isEmpty()){
                       Elements as = body.getElementsByTag("a");
                       for (Element a :as){
                           Local.urls.put(a.text(),a.attr("href"));
                           Log.i(TAG, "run: "+a.text()+":"+a.attr("href"));
                       }
-                  }*/
-                    Log.i(TAG, "run: "+body.text()+":");
+                  }else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    Log.i(TAG, "run: " + body.text() + ":" + cookie);
+                    picUpdate();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-
-
         }).start();
-
     }
-
+    private String massage(String msg){
+        String str="";
+        if (msg.indexOf("密码错误")!=-1){
+            str="如忘记密码，请与教务处联系！";
+        } else if (msg.indexOf("验证码不正确")!=-1){
+            str="验证码不正确";
+        }
+        return str;
+   }
 
 }
